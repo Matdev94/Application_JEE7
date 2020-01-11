97c5cd89907412760319339af410cdf7ebb00947
asus <asus@asus-PC>
2020-01-11 20:06:59 +0100

    e

---
+++ organisateur/build/build/src/agenda/modele/AgendaWrapper.java
@@ -0,0 +1,20 @@
+package agenda.modele;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+
+public class AgendaWrapper {
+	
+	private List<RendezVous> agenda ;
+	@XmlElement (name="rendez-vous")
+	public List<RendezVous> getAgenda() {
+	return agenda ;
+	}
+	public void setAgenda(List<RendezVous> agenda) {
+	
+
+	this.agenda = agenda ;  
+	}
+
+}
